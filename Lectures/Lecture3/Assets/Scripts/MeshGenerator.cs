8eff400806ce291520c78383f1ffc4e5601b8964
Vitalii.Selishchev <Vitalii.Selishchev@jetbrains.com>
2020-09-18 22:01:05 +0300

    Base part completed.

--- Lectures/Lecture3/Assets/Scripts/MeshGenerator.cs
+++ Lectures/Lecture3/Assets/Scripts/MeshGenerator.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
     public MetaBallField Field = new MetaBallField();
+
+    private const int CubesCount = 40;
+    private const float Eps = 0.01f;
     
     private MeshFilter _filter;
     private Mesh _mesh;
@@ -34,63 +39,103 @@ public class MeshGenerator : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        List<Vector3> cubeVertices = new List<Vector3>
-        {
-            new Vector3(0, 0, 0), // 0
-            new Vector3(0, 1, 0), // 1
-            new Vector3(1, 1, 0), // 2
-            new Vector3(1, 0, 0), // 3
-            new Vector3(0, 0, 1), // 4
-            new Vector3(0, 1, 1), // 5
-            new Vector3(1, 1, 1), // 6
-            new Vector3(1, 0, 1), // 7
-        };
+        var minX = Field.Balls.Min(t => t.position.x) - Field.BallRadius;
+        var maxX = Field.Balls.Max(t => t.position.x) + Field.BallRadius;
+        var minY = Field.Balls.Min(t => t.position.y) - Field.BallRadius;
+        var maxY = Field.Balls.Max(t => t.position.y) + Field.BallRadius;
+        var minZ = Field.Balls.Min(t => t.position.z) - Field.BallRadius;
+        var maxZ = Field.Balls.Max(t => t.position.z) + Field.BallRadius;
 
-        int[] sourceTriangles =
-        {
-            0, 1, 2, 2, 3, 0, // front
-            3, 2, 6, 6, 7, 3, // right
-            7, 6, 5, 5, 4, 7, // back
-            0, 4, 5, 5, 1, 0, // left
-            0, 3, 7, 7, 4, 0, // bottom
-            1, 5, 6, 6, 2, 1, // top
-        };
+        var min = Math.Min(minX, Math.Min(minY, minZ)) - 1;
+        var max = Math.Max(maxX, Math.Max(maxY, maxZ)) + 1;
 
+        var size = (max - min) / CubesCount;
         
+        var cubes = new List<Vector3[]>();
+
+        for (var i = min; i < max; i += size)
+        {
+            for (var j = min; j < max; j += size)
+            {
+                for (var k = min; k < max; k += size)
+                {
+                    cubes.Add(CreateCube(i, j, k, size));
+                }
+            }
+        }
+
         vertices.Clear();
         indices.Clear();
         normals.Clear();
         
         Field.Update();
-        // ----------------------------------------------------------------
-        // Generate mesh here. Below is a sample code of a cube generation.
-        // ----------------------------------------------------------------
 
-        // What is going to happen if we don't split the vertices? Check it out by yourself by passing
-        // sourceVertices and sourceTriangles to the mesh.
-        for (int i = 0; i < sourceTriangles.Length; i++)
+        foreach (var cube in cubes)
         {
-            indices.Add(vertices.Count);
-            Vector3 vertexPos = cubeVertices[sourceTriangles[i]];
-            
-            //Uncomment for some animation:
-            //vertexPos += new Vector3
-            //(
-            //    Mathf.Sin(Time.time + vertexPos.z),
-            //    Mathf.Sin(Time.time + vertexPos.y),
-            //    Mathf.Sin(Time.time + vertexPos.x)
-            //);
-            
-            vertices.Add(vertexPos);
+            var mask = 0;
+            for (var i = 0; i < cube.Length; i++)
+            {
+                if (Field.F(cube[i]) > 0)
+                {
+                    mask |= 1 << i;
+                }
+            }
+
+            var caseToTrianglesCount = MarchingCubes.Tables.CaseToTrianglesCount[mask];
+            var caseToVertices = MarchingCubes.Tables.CaseToVertices[mask];
+
+            for (var i = 0; i < caseToTrianglesCount; i++)
+            {
+                var edges = new[]
+                {
+                    caseToVertices[i].x,
+                    caseToVertices[i].y,
+                    caseToVertices[i].z
+                };
+                
+                for (var j = 0; j < 3; j++)
+                {
+                    var edgeEndpoints = MarchingCubes.Tables._cubeEdges[edges[j]];
+                    var first = cube[edgeEndpoints[0]];
+                    var second = cube[edgeEndpoints[1]];
+                    
+                    indices.Add(vertices.Count);
+                    var intersectionFraction = Field.F(second) / (Field.F(second) - Field.F(first));
+                    var vertex = intersectionFraction * first + (1 - intersectionFraction) * second;
+                    vertices.Add(vertex);
+
+                    var normal = new Vector3(
+                        Field.F(vertex + Eps * Vector3.right) - Field.F(vertex + Eps * Vector3.left),
+                        Field.F(vertex + Eps * Vector3.up) - Field.F(vertex + Eps * Vector3.down),
+                        Field.F(vertex + Eps * Vector3.forward) - Field.F(vertex + Eps * Vector3.back)
+                    );
+                    normals.Add(-normal.normalized);
+                }
+            }
         }
 
         // Here unity automatically assumes that vertices are points and hence (x, y, z) will be represented as (x, y, z, 1) in homogenous coordinates
         _mesh.Clear();
         _mesh.SetVertices(vertices);
         _mesh.SetTriangles(indices, 0);
-        _mesh.RecalculateNormals(); // Use _mesh.SetNormals(normals) instead when you calculate them
+        _mesh.SetNormals(normals);
 
         // Upload mesh data to the GPU
         _mesh.UploadMeshData(false);
+    }
+
+    private Vector3[] CreateCube(float x, float y, float z, float size)
+    {
+        return new[]
+        {
+            new Vector3(x, y, z), // 0
+            new Vector3(x, y + size, z), // 1
+            new Vector3(x + size, y + size, z), // 2
+            new Vector3(x + size, y, z), // 3
+            new Vector3(x, y, z + size), // 4
+            new Vector3(x, y + size, z + size), // 5
+            new Vector3(x + size, y + size, z + size), // 6
+            new Vector3(x + size, y, z + size), // 7
+        };
     }
 }
